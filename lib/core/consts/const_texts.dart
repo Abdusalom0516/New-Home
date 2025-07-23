1c93fe11ce1bba3cc781bef415b64312ce224b96
Abdusalom G'ayratov <abdusalomgayratov7@gmail.com>
2025-07-24 00:37:38 +0500

    BASE: Create ConstText Class Values.

--- lib/core/consts/const_texts.dart
+++ lib/core/consts/const_texts.dart
@@ -1,3 +1,60 @@
 class ConstTexts {
-  
+  String helloName = "Hello Sarthak!";
+  String readyToRescue = "Ready to Rescue? ";
+  String adotAPet = "Adopt a Pet";
+  String lostAndFound = "Lost & Found";
+  String browseAnimals = "Browse animals available for adoption.";
+  String repostList = "Report a lost pet or a found animal.";
+  String seeAll = "See all";
+  String exploreWaysToHelp = "Explore ways to help";
+  String favorites = "Favorites";
+  String pet = "Pet";
+  String articles = "Articles";
+  String love = "Unconditional Love: The Joy of Pet Companionship";
+  String by = "By";
+  String images = "Images";
+  String profile = "Profile";
+  String searchLostAndFound = "Search Lost & Found Pets...";
+  String lostAndFoundForm = "Lost & Found Form";
+  String typeOfPet = "Type of Pet";
+  String dog = "Dog";
+  String cat = "Cat";
+  String other = "Other (e.g., rabbit, bird)";
+  String description = "Description";
+  String descriptionHint =
+      "Please describe your lost pet (e.g., breed, color, size, distinctive markings)";
+  String dateAndTime = "Date & Time";
+  String dateAndTimeHint = "What’s the date & time when you found the pet?";
+  String location = "Location";
+  String locationHint = "Where did you find the pet?";
+  String contactInfo = "Contact Info*";
+  String contactInfoHint = "Your Phone Number";
+  String uploadPhoto = "Upload Photo*";
+  String found = "Found";
+  String lost = "Lost";
+  String submit = "Submit";
+  String reportLostOrFound = "Report Lost or Found";
+  String petDetails = "Pet Details";
+  String gender = "Gender";
+  String age = "Age";
+  String size = "Size";
+  String aboutPet = "About Pet";
+  String lorem =
+      "Figma ipsum component variant main layer. Style boolean italic star pixel mask underline. Union object main slice team align. Ellipse blur pixel fill rotate text. Duplicate inspect figma scale content move edit distribute asset. Inspect union create opacity strikethrough. Rectangle layout ipsum selection line connection export italic ipsum. Asset polygon rectangle component vertical invite pen ipsum. Duplicate hand comment editor star community strikethrough rotate share polygon. Content asset duplicate team strikethrough link fill.";
+  String email = "Email";
+  String password = "Password";
+  String forgotPassword = "Forgot Password?";
+  String passwordError = "Password will be greater then 6 digit.";
+  String signIn = "Sign In";
+  String doNothaveAcc = "you don’t have any account?";
+  String signUpNow = "sign up now";
+  String accountSetUp = "Account Setup!";
+  String name = "Name";
+  String nameHint = "What’s your full name";
+  String emailHint = "What’s your email address";
+  String passwordHint = "Create a password for your account";
+  String backtoSignIn = "Back to Sign In";
+  String userPref = "User Preferences!";
+  String next = "Next";
+  String back = "Back";
 }
