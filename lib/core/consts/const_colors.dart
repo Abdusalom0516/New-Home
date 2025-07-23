85fc53ac14511977aa8708d73edcf1fbb9d58956
Abdusalom G'ayratov <abdusalomgayratov7@gmail.com>
2025-07-23 18:07:12 +0500

    INFRA: Create ConstColors Class.

--- lib/core/consts/const_colors.dart
+++ lib/core/consts/const_colors.dart
@@ -1,1 +1,11 @@
-class ConstColors {}
+import 'package:flutter/material.dart';
+
+class ConstColors {
+  Color ffFFFFFF = Color(0xFFFFFFFF);
+  Color ffE8F7F6 = Color(0xFFE8F7F6);
+  Color ff16A99F = Color(0xFF16A99F);
+  Color ff373737 = Color(0xFF373737);
+  Color ff000000 = Color(0xFF000000);
+  Color ffF6F6F6 = Color(0xFFF6F6F6);
+  Color ffE3E3E3 = Color(0xFFE3E3E3);
+}
