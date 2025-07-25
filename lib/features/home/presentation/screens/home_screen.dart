fb9abe86fc7001e2b06e7fc957462624f2b3c8d1
Abdusalom G'ayratov <abdusalomgayratov7@gmail.com>
2025-07-25 07:49:47 +0500

    BASE: Create Home Screen UI.

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:small_kindness/core/consts/const_text_styles.dart';
+import 'package:small_kindness/core/utils/app_state_wrapper.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return AppStateWrapper(
+      builder: (colors, texts, images) => Scaffold(
+        body: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        texts.helloName,
+                        style: AppTextStyles.urbanist.regular(
+                          color: colors.ff000000,
+                          fontSize: 18.sp,
+                        ),
+                      ),
+                      Text(
+                        texts.readyToRescue,
+                        style: AppTextStyles.urbanist.semiBold(
+                          color: colors.ff000000,
+                          fontSize: 22.sp,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/features/main/main_screen.dart
+++ lib/features/main/main_screen.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:small_kindness/features/home/presentation/screens/home_screen.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends HookWidget {
   const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final pageController = usePageController(initialPage: 0);
     return Scaffold(
       body: PageView(
-        children: [],
+        controller: pageController,
+        physics: NeverScrollableScrollPhysics(),
+        children: [HomeScreen(), HomeScreen(), HomeScreen(), HomeScreen()],
       ),
     );
   }
