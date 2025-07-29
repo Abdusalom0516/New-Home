42cdb5869c50b10773a09b08b1f1e93c382d5211
Abdusalom G'ayratov <abdusalomgayratov7@gmail.com>
2025-07-29 11:05:29 +0500

    BASE: Implement Clean Architecture.

--- lib/features/articles/presentation/screens/full_article_screen.dart
+++ lib/features/articles/presentation/screens/full_article_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:small_kindness/core/consts/const_colors.dart';
 import 'package:small_kindness/core/consts/const_text_styles.dart';
 import 'package:small_kindness/core/consts/const_texts.dart';
 import 'package:small_kindness/core/utils/app_network_image.dart';
@@ -21,71 +22,80 @@ class FullArticleScreen extends HookWidget {
             // AppBarSection
             appBarSection(texts),
             SliverHeight(height: 5),
-            SliverPadding(
-              padding: EdgeInsetsGeometry.only(left: 16.r, right: 7.r),
-              sliver: SliverFillRemaining(
-                child: Scrollbar(
-                  controller: scrollController,
-                  interactive: true,
-                  radius: Radius.circular(8.r),
+            // Article Section
+            articleSection(scrollController, colors, texts),
+          ],
+        ),
+      ),
+    );
+  }
 
-                  child: SingleChildScrollView(
-                    controller: scrollController,
-                    child: Padding(
-                      padding: EdgeInsetsGeometry.only(right: 9.r),
-                      child: Column(
-                        children: [
-                          Text(
-                            '"Why puppies are so cute? How to teach them to poop?"',
-                            style: AppTextStyles.urbanist.semiBold(
-                              color: colors.ff000000,
-                              fontSize: 21.sp,
-                            ),
-                          ),
-                          Height(height: 15),
-                          ClipRRect(
-                            borderRadius: BorderRadiusGeometry.circular(8.r),
-                            child: AppNetworkImage(
-                              imageUrl:
-                                  "https://i.pinimg.com/1200x/2c/a3/e9/2ca3e90e40e4b10ddb1ed04340eb453b.jpg",
-                              height: 200.h,
-                              width: double.infinity,
-                            ),
-                          ),
-                          Height(height: 15),
-                          Text(
-                            texts.lorem +
-                                texts.lorem +
-                                texts.lorem +
-                                texts.lorem +
-                                texts.lorem +
-                                texts.lorem,
-                            style: AppTextStyles.urbanist.regular(
-                              color: colors.ff000000,
-                              fontSize: 16.sp,
-                            ),
-                          ),
-                          Height(height: 25),
-                          Row(
-                            children: [
-                              Text(
-                                "29/07/2025",
-                                style: AppTextStyles.urbanist.semiBold(
-                                  color: colors.ff000000,
-                                  fontSize: 19.sp,
-                                ),
-                              ),
-                            ],
-                          ),
-                          Height(height: 45),
-                        ],
-                      ),
+  SliverPadding articleSection(
+    ScrollController scrollController,
+    ConstColors colors,
+    ConstTexts texts,
+  ) {
+    return SliverPadding(
+      padding: EdgeInsetsGeometry.only(left: 16.r, right: 7.r),
+      sliver: SliverFillRemaining(
+        child: Scrollbar(
+          controller: scrollController,
+          interactive: true,
+          radius: Radius.circular(8.r),
+
+          child: SingleChildScrollView(
+            controller: scrollController,
+            child: Padding(
+              padding: EdgeInsetsGeometry.only(right: 9.r),
+              child: Column(
+                children: [
+                  Text(
+                    '"Why puppies are so cute? How to teach them to poop?"',
+                    style: AppTextStyles.urbanist.semiBold(
+                      color: colors.ff000000,
+                      fontSize: 21.sp,
                     ),
                   ),
-                ),
+                  Height(height: 15),
+                  ClipRRect(
+                    borderRadius: BorderRadiusGeometry.circular(8.r),
+                    child: AppNetworkImage(
+                      imageUrl:
+                          "https://i.pinimg.com/1200x/2c/a3/e9/2ca3e90e40e4b10ddb1ed04340eb453b.jpg",
+                      height: 200.h,
+                      width: double.infinity,
+                    ),
+                  ),
+                  Height(height: 15),
+                  Text(
+                    texts.lorem +
+                        texts.lorem +
+                        texts.lorem +
+                        texts.lorem +
+                        texts.lorem +
+                        texts.lorem,
+                    style: AppTextStyles.urbanist.regular(
+                      color: colors.ff000000,
+                      fontSize: 16.sp,
+                    ),
+                  ),
+                  Height(height: 25),
+                  Row(
+                    children: [
+                      Text(
+                        "29/07/2025",
+                        style: AppTextStyles.urbanist.semiBold(
+                          color: colors.ff000000,
+                          fontSize: 19.sp,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Height(height: 45),
+                ],
               ),
             ),
-          ],
+          ),
         ),
       ),
     );
