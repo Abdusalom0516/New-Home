097d479368b97c8664aa034877612331785ed949
Abdusalom G'ayratov <abdusalomgayratov7@gmail.com>
2025-07-29 18:20:57 +0500

    BASE: Implement Clean Code.

--- lib/features/add/presentation/screens/lost_and_found_add_screen.dart
+++ lib/features/add/presentation/screens/lost_and_found_add_screen.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:small_kindness/core/consts/const_text_styles.dart';
 import 'package:small_kindness/core/consts/const_texts.dart';
 import 'package:small_kindness/core/utils/app_state_wrapper.dart';
 import 'package:small_kindness/core/widgets/custom_coming_soon_wd.dart';
 import 'package:small_kindness/core/widgets/custom_sliver_height_wd.dart';
+import 'package:small_kindness/features/add/presentation/widgets/lost_or_found_card_wd.dart';
 
 class LostAndFoundAddScreen extends HookWidget {
   const LostAndFoundAddScreen({super.key});
@@ -59,61 +59,6 @@ class LostAndFoundAddScreen extends HookWidget {
       pinned: true,
       floating: true,
       snap: true,
-    );
-  }
-}
-
-class LostOrFoundCard extends StatelessWidget {
-  const LostOrFoundCard({
-    super.key,
-    required this.image,
-    required this.title,
-    required this.func,
-    required this.isChosen,
-  });
-  final String image, title;
-  final VoidCallback func;
-  final bool isChosen;
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: AppStateWrapper(
-        builder: (colors, texts, images) => InkWell(
-          onTap: func,
-          child: Column(
-            spacing: 7.h,
-            children: [
-              Container(
-                height: 99.h,
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: isChosen ? colors.ff000000 : colors.transparent,
-                    width: 2.7.r,
-                  ),
-                  borderRadius: BorderRadius.circular(8.r),
-                ),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(5.r),
-                  child: Image.asset(
-                    width: double.infinity,
-                    height: 99.h,
-                    image,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-              Text(
-                title,
-                style: AppTextStyles.urbanist.bold(
-                  color: colors.ff000000,
-                  fontSize: 16.sp,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
     );
   }
 }
